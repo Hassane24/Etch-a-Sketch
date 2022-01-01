c7e79a48561f16d914962e9afce8cc62dfc2814e
Hassane24 <hsisane5@gmail.com>
2022-01-01 14:35:31 +0100

    finished

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const container = document.getElementById("container");
-function addDivs() {
+
+function addDivs(rows, columns) {
   let div;
-  for (let i = 0; i < 256; i++) {
+  columns = prompt("Choose the gridsize");
+  rows = columns;
+  let size = 100 / columns;
+  container.setAttribute(
+    "style",
+    `display: grid;
+     grid-template-rows: repeat(${rows}, ${size}%);
+     grid-template-columns: repeat(${columns}, ${size}%);`
+  );
+  if (rows == 100 || columns == 100) return alert("ERROR, Don't go above 100");
+  for (let i = 0; i < rows * columns; i++) {
     div = document.createElement("div");
     div.classList.add("etch");
     container.appendChild(div);
@@ -16,3 +27,11 @@ for (let i = 0; i < divs.length; i++) {
     divs[i].classList.add("hovery");
   });
 }
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  for (i = 0; i < divs.length; i++) {
+    divs[i].classList.remove("hovery");
+  }
+  addDivs();
+});
